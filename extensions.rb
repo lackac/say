01b10b225d5e399d9d3ce8b604ac3fdc6343fa70
Laszlo Bacsi <lackac@lackac.hu>
2009-04-03 00:32:13 +0200

    Make slugify Ruby1.8.6 compatible

--- extensions.rb
+++ extensions.rb
@@ -6,14 +6,14 @@ class String
   ]
 
   def slugify
-    self.dup.tap do |str|
-      ACCENTS_TR.first.each_with_index do |char, i|
-        str.gsub!(char, ACCENTS_TR.last[i])
-      end
-      str.gsub!(/[^a-zA-Z0-9_ -]/, "")
-      str.gsub!(/[ _-]+/, " ")
-      str.strip!
-      str.tr!(' ', "-")
+    slug = self.dup
+    ACCENTS_TR.first.each_with_index do |char, i|
+      slug.gsub!(char, ACCENTS_TR.last[i])
     end
+    slug.gsub!(/[^a-zA-Z0-9_ -]/, "")
+    slug.gsub!(/[ _-]+/, " ")
+    slug.strip!
+    slug.tr!(' ', "-")
+    slug
   end
 end
