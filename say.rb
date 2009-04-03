be1ed243cf92af97cf69a4ca72269badcbc92fba
Laszlo Bacsi <lackac@lackac.hu>
2009-04-03 10:41:41 +0200

    Some security fixes

--- say.rb
+++ say.rb
@@ -7,13 +7,17 @@ require 'iconv'
 configure do
   set :espeak, "/usr/bin/espeak"
   set :say_tmp_path, Pathname.new(__FILE__).dirname.join("tmp", "cache")
+  set :allowed_chars, ['a'..'z', 'A'..'Z', '0'..'9', ',', '.', '?', '!', String::ACCENTS].map {|r| Array(r)}.flatten
 end
 configure :development do
   set :espeak, "/Users/LacKac/bin/espeak"
 end
 
-get %r{^/((?:mb/)?[^/]+)/(.*)$} do |voice, text|
+get %r{^/([a-z-]+)/(.*)$} do |voice, text|
   begin
+    text = text.scan(/./u).map do |c|
+      options.allowed_chars.include?(c) ? c : ' '
+    end.join("").strip.squeeze
     file = options.say_tmp_path.join(voice, "#{text.slugify}.wav")
     unless file.cleanpath.to_s.start_with?(options.say_tmp_path.cleanpath.to_s)
       raise RuntimeError, "Unreachable path"
